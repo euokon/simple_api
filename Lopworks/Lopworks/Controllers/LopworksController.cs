83a5e9f1ba3f7139e567ca64668cc2fb654fbe77
eokon <eokon@unionbankng.com>
2022-06-30 09:59:49 +0100

    simple API

---
+++ Lopworks/Lopworks/Controllers/LopworksController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lopworks.Controllers
+{
+    [Route("api/lopworks")]
+    [ApiController]
+    public class LopworksController : ControllerBase
+    {
+        [HttpGet("")]
+        public string GetFirstResponse()
+        {
+            var newData = "Kindly insert a query parameter";
+            return newData;
+        }
+
+        [HttpGet("{queryParameter}")]
+        public string GetFirstResponse(string queryParameter)
+        {
+            var newData = "";
+            if (queryParameter == "name")
+            {
+                newData = "My name is Emmanuel";
+            }
+            else if (queryParameter == "job")
+            {
+                newData = "I work as a developer";
+            }
+
+            return newData;
+        }
+    }
+}
---
+++ Lopworks/Lopworks/LopworksData/DataObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lopworks.LopworksData
+{
+    public class DataObject
+    {
+        public string ResponseData { get; set; }
+    }
+}
